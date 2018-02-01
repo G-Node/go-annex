17f49ab1e278c8ed4743572c0538a38678e3bebe
cgars <christian@stuebeweg50.de>
2018-02-01 15:03:19 +0100

    Annex file matching before lookup -> faster

--- file.go
+++ file.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"fmt"
 	"log"
+	"regexp"
 )
 
 type AFile struct {
@@ -21,9 +22,28 @@ type AnnexFileNotFound struct {
 
 func NewAFile(annexpath, repopath, Ofilename string, APFileC []byte) (*AFile, error) {
 	nAF := &AFile{OFilename: Ofilename}
+	secPar := regexp.MustCompile(`(\.\.)`)
+	if secPar.Match(APFileC){
+		return nil, fmt.Errorf("Path not allowed")
+	}
+	// see https://regexper.com/#%5B%5C%5C%5C%2F%5Dannex%5B%5C%5C%5C%2F%5D(%5B%5E%5C.%5D%2B(%5C.%5Cw%2B))%3F
+	aFPattern := regexp.MustCompile(`[\\\/]annex[\\\/](.+)`)
+	matches := aFPattern.FindStringSubmatch(string(APFileC))
+	log.Printf("matched: %v", matches)
+	if matches != nil && len(matches) > 1 {
+		filepath := strings.Replace(matches[1], "\\", "/", 0)
+		filepath = fmt.Sprintf("%s/annex/%s", repopath, filepath)
+		log.Printf("Filepath: %s", filepath)
+		info, err := os.Stat(filepath)
+		if err == nil {
+			nAF.Filepath = filepath
+			nAF.Info = info
+			return nAF, nil
+		}
+	}
+
 	pathParts := strings.SplitAfter(string(APFileC), string(os.PathSeparator))
 	filename := strings.TrimSpace(pathParts[len(pathParts)-1])
-
 	// lets find the annex file
 	filepath.Walk(filepath.Join(annexpath, repopath), func(path string, info os.FileInfo, err error) error {
 		if err != nil {
